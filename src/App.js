c07ab5afdaf08cab28c03375d105f770d7aa36f1
qwertyboy0325 <ezra40907@gmail.com>
2022-06-10 12:16:13 +0800

    Create Playground Page.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import './App.css';
+import { Home } from './components/home';
+import { NavBar } from './components/NavBar'
+import { MusicEditor } from './components/musicEditor';
 
 function App() {
   return (
+    // <React.Fragment>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<MusicEditor />} />
+        {/* <Route path="playground" element={<MusicEditor />} /> */}
+      </Routes>
     </div>
+
+    // </React.Fragment>
   );
 }
 
