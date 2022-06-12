b27d373ab4e8118272d7be347ea97eb4ac095aab
qwertyboy0325 <ezra40907@gmail.com>
2022-06-12 18:53:01 +0800

    add avatar hover and click action.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,33 +2,48 @@ import React, { useEffect, useRef, useState } from "react";
 import './NavBar.css'
 
 export function NavBar() {
-    const [user_avatar, user_avatar_isHover] = useHover();
-    const [user_info, user_info_isHover] = useHover();
+
+    // const [user_avatar, user_avatar_isHover] = useHover();
+    // const [user_info, user_info_isHover] = useHover();
+    const userInfoContentRef = useRef(null)
+    const userInfoRef = useRef(null);
+    const triggerRef = useRef(null);
+    const triggerEvent = {
+        onMouseHandler: () => {
+            let userInfo = userInfoRef.current;
+            userInfo.classList.add("hover");
+        },
+        onClickHandler: () => {
+            // let userInfoContent = userInfoContentRef.current;
+            let userInfo = userInfoRef.current;
+            let trigger = triggerRef.current;
+            trigger.style.display = "none";
+            userInfo.classList.remove("hover");
+            userInfo.classList.add("open");
+        },
+        onMouseLeaveHandler: () => {
+            let userInfo = userInfoRef.current;
+            userInfo.classList.remove("hover");
+        }
+    }
+
     return (
         <div>
             <div id="navbar">
-                <div id="user-avatar" ref={user_avatar} style={{ opacity: user_avatar_isHover || user_info_isHover ? '0%' : '100%' }}></div>
-            </div>
-            <div id="trigger">
-                <div id="user-info" ref={user_info} style={{ display: user_avatar_isHover || user_info_isHover ? 'block' : 'none' }}>
-                    <div id="user-avatar" style={{ right: '35%', top: '90%' }}></div>
+                <div id="avatar-trigger" className="trigger"
+                    onMouseEnter={triggerEvent.onMouseHandler}
+                    onClick={triggerEvent.onClickHandler}
+                    onMouseLeave={triggerEvent.onMouseLeaveHandler}
+                    ref={triggerEvent.triggerRef}></div>
+                <div className="user-info" ref={userInfoRef}>
+                    <div className="user-avatar"  ></div>
+                    <div className="user-info-content" ref={userInfoContentRef} >
+                        {/* <div id="user-avatar" style={{ right: '35%', top: '90%' }}></div> */}
+
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
 
-const useHover = () => {
-    const [value, setValue] = useState(false);
-    const ref = useRef(null);
-    const handleMouseOver = () => setValue(true);
-    const handleMouseOut = () => setValue(false);
-    useEffect(() => {
-        const node = ref.current;
-        if (node) {
-            node.addEventListener('mouseover', handleMouseOver);
-            node.addEventListener('mouseout', handleMouseOut);
-        }
-    });
-    return [ref, value];
-}
